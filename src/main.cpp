215d89e51e77a89b24937d9076d267f94f4c063e
Artemiy Leonov <leonovartemii@gmail.com>
2025-02-25 17:54:00 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "utils.cpp"	
+
+int main() {
+	int x = 10;
+	int y = 5;
+	std::cout << Addition(x, y) << " " << Substraction(x, y);
+}
---
+++ src/utils.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int Addition(int a, int b) {
+	return (a + b);
+}
+
+int Substraction(int a, int b) {
+	return (a - b);
+}
